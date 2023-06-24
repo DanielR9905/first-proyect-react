e26f2fa3c7648483aeff73899921cbee957ab800
DanielR9905 <danielramirezduque1@gmail.com>
2023-06-23 19:57:33 -0500

    Cards 100%

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,21 @@
 import React from "react";
+import '../Cards/Cards.css'
 
-export default function Card(props) {
+export default function Card({title, imageSource, extract, url, targetBlank}) {
   return (
     <>
         <div className="card text-center bg-dark">
-            <img src=""/>
+            <div className="contain-overflow">
+              <img src={imageSource}  className="img-featured" alt=""/>
+            </div>
             <div className="card-body text-light">
-                <h4 className="card-title">{props.title}</h4>
-                <p className="card-text text-secondary">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea dolorum atque voluptatibus! Asperiores libero esse eligendi assumenda labore consequuntur modi tempore deserunt optio quo velit earum, sapiente odit eius ut.</p>
-                <a href="#!" className="btn btn-outline-secondary">Go to this website</a>
+                <h4 className="caZrd-title">{title}</h4>
+                <p className="card-text text-secondary">
+                  {
+                    extract ? extract : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea dolorum atque voluptatibus! Asperiores libero esse eligendi assumenda labore consequuntur modi tempore deserunt optio quo velit earum, sapiente odit eius ut'
+                  }
+                  </p>
+                <a href={url} className="btn btn-outline-secondary" target={targetBlank}>Go to this website</a>
             </div>
         </div>
     </>
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -10,16 +10,25 @@ const cards = [
     id: 1,
     title: "DR Website",
     image: image1,
+    extract: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae enim itaque ex.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae enim itaque ex.',
+    url: 'https://DanielR9905.github.io/1',
+    blank: '_blank'
   },
   {
     id: 1,
     title: "DR Blog",
     image: image2,
+    extract: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae enim itaque ex.',
+    url: 'https://DanielR9905.blog.io',
+    blank: '_blank'
   },
   {
     id: 1,
     title: "DR CV",
     image: image3,
+    extract: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae enim itaque ex.Quae enim itaqueQuae enim itaque',
+    url: 'https://DanielR9905.github.io/2',
+    blank: '_blank'
   },
 ];
 
@@ -30,7 +39,7 @@ export default function Cards() {
         <div className="row">
           {cards.map(card => (
             <div className="col-md-4" key={card.id}>
-              <Card title={card.title}/>
+              <Card title={card.title} imageSource={card.image} url={card.url} targetBlank={card.blank} extract={card.extract}/>
             </div>
           ))}
         </div>
